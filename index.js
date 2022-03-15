eccb88c3a8979c7bb39ab50a22fd270115ad04e9
Gian <s.gianmarco@gmail.com>
2022-03-15 13:40:54 +0100

    commit message wrong

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-console.log('starting');
-console.log('starting2');
-console.log('starting3');
-console.log('starting4');
-console.log('starting5');
-console.log('starting6');
-console.log('starting7');
-console.log('starting8');
+const start = async () => {
+    try {
+        console.log('starting');
+        let res = await test1();
+        console.log(res);
+        // await test1();
+    } catch (e) {
+        console.log('ERROR***:', e);
+    }
+}
+
+const test1 = async () => {
+    console.log('test1');
+    return test2();
+};
+
+
+
+const test2 = () => {
+    return new Promise((resolve, reject) => {
+        reject(new Error('test2'))
+    })
+
+    // try {
+    //     console.log('test2');
+    //     throw new Error('test2');
+    // }catch (e) {
+    //     return e;
+    // }
+    // console.log('test2');
+    // return new Error('test2');
+}
+
+start()
+    .then((result) => {
+        console.log('final result');
+        console.log(result);
+    })
+    .catch((err) => {
+        console.log('final err');
+        console.log(err);
+    });
+
