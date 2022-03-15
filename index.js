0c5ae2437ac77e47724049ead7f439ec5adb4f30
Gian <s.gianmarco@gmail.com>
2022-03-15 13:49:01 +0100

    refactor: prevent racing of requests
    Introduce a request id and a reference to latest request. Dismiss
    incoming responses other than from latest request.

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ const test1 = async () => {
 
 
 const test2 = () => {
-    return new Promise((resolve, reject) => {
-        reject(new Error('test2'))
-    })
-
+    // return new Promise((resolve, reject) => {
+    //     reject(new Error('test2'))
+    // })
+    //
     // try {
     //     console.log('test2');
     //     throw new Error('test2');
     // }catch (e) {
     //     return e;
     // }
-    // console.log('test2');
-    // return new Error('test2');
+    console.log('test2');
+    return new Error('test2');
 }
 
 start()
